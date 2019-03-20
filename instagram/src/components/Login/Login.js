b9c9963528a88a8b139a5513259e364ae8ef00c4
Kristea Gamel <krgamel@gmail.com>
2019-03-19 22:24:55 -0500

    created Login
    haven’t linked it up to App.js yet

---
+++ instagram/src/components/Login/Login.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import './Login';
+
+class Login extends Component {
+    constructor(props) {
+        super(props); 
+        this.state = {
+            username: '',
+            password: ''
+        };
+    }
+
+    render() {
+        return (
+            <form className="login">
+                <h3>InstaClone</h3>
+                <p>Please Login</p>
+                <div className="login-user">
+                    <input
+                        text="text"
+                        placeholder="Username"
+                        name="username"
+                        value={this.state.username}
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div className="login-password">
+                    <input 
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                    />
+                    <br />
+
+                    <button onClick={this.handleLogin}>
+                        Log In
+                    </button>
+                </div>
+            </form>
+        );
+    }
+}
+
+
+export default Login;
