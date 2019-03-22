a2dd246dfb1e48810dffbb09d400a17f35d4a3c8
Kristea Gamel <krgamel@gmail.com>
2019-03-22 15:29:16 -0500

    styled components for Post

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import CommentSection from '../CommentSection/CommentSection';
-import './PostContainer.css';
+// import './PostContainer.css';
 import heart from '../../img/heart-regular.svg';
 import solidHeart from '../../img/heart-solid.svg';
-
+import styled from 'styled-components';
 
 // const PostContainer = props => {
 //     // console.log(props)
@@ -26,6 +26,64 @@ import solidHeart from '../../img/heart-solid.svg';
 //     )
 // };
 
+
+const Post = styled.div `
+    border: 1px solid lightgrey;
+    height: auto;
+    width: 100%; 
+    margin-bottom: 50px;
+
+    i {
+      width: 20px;
+      height: 20px;
+    }
+`;
+
+const PostHeader = styled.div `
+    display: flex;
+    margin: 10px; 
+    align-content: center;
+    align: center;
+`;
+
+const ProfileImg = styled.img `
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+`;
+
+const UserName = styled.p `
+  font-weight: bold;
+  margin-top: 15px;
+  margin-left: 5px;
+  align-content: center;
+`;
+
+const PostImg = styled.img `
+  width: 100%;
+`;
+
+const LikesImg = styled.img `
+  
+  margin-right: 10px;
+`;
+
+const LikeCount = styled.p `
+ text-align: left;
+font-weight: bold;
+margin-left: 10px;
+
+`;
+
+const PostFooter = styled.div `
+display: flex;
+margin-top: 5px;
+margin-left: 10px;
+align-content: center;
+align-items: center;
+
+`;
+const TimeStamp = styled.p ``;
 class PostContainer extends Component {
     constructor(props) {
         super(props)
@@ -53,20 +111,23 @@ class PostContainer extends Component {
       }
     render() {
         return (
-            <div className="post-container">
-             <div className="post-header">
-                 <img className="profile" src={this.state.data.thumbnailUrl} alt=""/>
-                 <p className="username">{this.state.data.username}</p>
-             </div>
-             <img className="image" src={this.state.data.imageUrl} alt="" />
-                 <div className="likes">
-                 <img src={this.state.likeStatus ? solidHeart : heart} alt="heart" width="20" onClick={this.clickedImg}/>
-                 <i className="far fa-comment"></i>
-                 <p className='likes'>{this.state.likes} likes</p>
-              </div>
+            <Post>
+             <PostHeader>
+                 <ProfileImg src={this.state.data.thumbnailUrl} alt=""/>
+                 <UserName>{this.state.data.username}</UserName>
+             </PostHeader>
+             <PostImg src={this.state.data.imageUrl} alt="" />
+                
+                <PostFooter>
+                  <LikesImg src={this.state.likeStatus ? solidHeart : heart} alt="heart" height="20" width="20" onClick={this.clickedImg}/>
+                  <i className="far fa-comment"></i>
+                </PostFooter>
+                <LikeCount>{this.state.likes} likes</LikeCount>
+
             <CommentSection comments={this.state.data.comments} data={this.state.data} />
-            <p className="timestamp">{this.state.timestamp}</p>   
-         </div> 
+            
+            <TimeStamp>{this.state.timestamp}</TimeStamp>   
+         </Post> 
         )
     }
 }
