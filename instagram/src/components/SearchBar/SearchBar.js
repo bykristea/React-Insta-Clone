f2144a1ea41919791faaccde13ab5a8eac6cf7b4
Kristea Gamel <krgamel@gmail.com>
2019-03-22 13:20:53 -0500

    used styled components for search bar

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import logo from './../../img/Instagram_logo.png';
+import camera from './../../img/camera-retro-solid.svg';
 import styled from 'styled-components';
 
  const Header = styled.header `
     border-bottom: 1px solid #3333;
     height: 60px;
     display: flex;
+    align-items: center;
+    
  `;
 
  const LogoHeader = styled.div`
-
+  display: flex;
+  align-content: center;
+  align-items: center;
+  margin-left: 10px;
 `;
 
 const LogoImage = styled.div`
@@ -18,7 +24,7 @@ const LogoImage = styled.div`
 `;
 
 const Divider = styled.span`
- 
+  height: 30px;
 `;
 
 const InputWrapper = styled.div`
@@ -29,11 +35,17 @@ const SearchInput = styled.input`
   height: 30px;
   width: 300px;
   text-align: center;
-  margin-right: 20px;
+  margin-left: 70px;
+  margin-right: 70px;
 `;
 
 const IconsWrapper = styled.div`
-  
+  display: flex;
+  justify-content: space-between;
+
+    i {
+      margin: 0 10px;
+    }
 `;
 const SearchIcon = styled.i`
   
@@ -46,10 +58,10 @@ const Search = (props) => {
         <Header>
         <LogoHeader>
           <LogoImage>
-            <i className="fas fa-camera-retro" />
+            <img src={camera} width="20px"/>
           </LogoImage>
           <Divider>│</Divider>
-          <LogoImage>Insta</LogoImage>
+          <img src={logo} width="100px"/>
         </LogoHeader>
 
         <SearchInput type="text" name='searchPosts' value={props.text} onChange={ props.changed} placeholder="🔍 Search" />
